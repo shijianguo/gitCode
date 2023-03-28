cb2e817ff569e4740144c48874b63d183d7a6687
sjg <1059387132@qq.com>
2023-03-28 10:03:27 +0800

    git-commit-springboot

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("this is spring boot starter");
         System.out.println("this is spring boot end");
+        System.out.println("this is springBoot detail start");
         SpringApplication.run(DemoApplication.class, args);
     }
 
